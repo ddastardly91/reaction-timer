e1ae3fa5cf7e956ef6dd91e4d30a9d32a038c249
Aaron Latham <alatham7891@gmail.com>
2022-10-18 15:59:27 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Reaction Timer</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+
+  data() {},
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>

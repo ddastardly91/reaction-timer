d094598c34c1ff91b7968f3ca710b5de755a01d5
Aaron Latham <alatham7891@gmail.com>
2022-10-18 16:38:40 +0100

    ADDED: Timer for block to appear and Interval to count reaction

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="block">CLICK ME</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">CLICK ME</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+    },
+  },
 };
 </script>
 
